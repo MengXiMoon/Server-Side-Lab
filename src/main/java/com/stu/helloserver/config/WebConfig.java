1edee30e2bdd337b36a906ef048eb4eb372d7898
梦溪 <1269057348@qq.com>
2026-04-09 14:51:39 +0800

    第六周

--- src/main/java/com/stu/helloserver/config/WebConfig.java
+++ src/main/java/com/stu/helloserver/config/WebConfig.java
@@ -12,6 +12,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new AuthInterceptor())
                 .addPathPatterns("/api/**")
-                .excludePathPatterns("/api/users/login"); // 只在这里放行登录，其他全权交由拦截器
+                .excludePathPatterns("/api/users/login", "/api/users", "/api/users/page"); // 放行登录、注册和分页查询
     }
 }
